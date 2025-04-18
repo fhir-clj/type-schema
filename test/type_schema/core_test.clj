78247492b1d48c93386343306a12577eef10e7f2
Aleksandr Penskoi <alexandr.penskoi@health-samurai.io>
2025-04-18 09:50:21 +0200

    [misc] Use HealthSamurai/golden-clj instead local implementation.

--- test/type_schema/core_test.clj
+++ test/type_schema/core_test.clj
@@ -21,43 +21,43 @@
 (deftest fhir-schema->type-schema-golden-test
   (package/init-from-package! "hl7.fhir.r4.core")
 
-  (golden/vs-json "test/golden/element.ts.json"
+  (golden/as-json "test/golden/element.ts.json"
                   (fhir-schema->json "test/golden/element.fs.json"))
 
-  (golden/vs-json "test/golden/backbone-element.ts.json"
+  (golden/as-json "test/golden/backbone-element.ts.json"
                   (fhir-schema->json "test/golden/backbone-element.fs.json"))
 
-  (golden/vs-json "test/golden/bundle.ts.json"
+  (golden/as-json "test/golden/bundle.ts.json"
                   (fhir-schema->json "test/golden/bundle.fs.json"))
 
-  (golden/vs-json "docs/examples/string.ts.json"
+  (golden/as-json "docs/examples/string.ts.json"
                   (fhir-schema->json "docs/examples/fhir-schema/string.fs.json"))
 
-  (golden/vs-json "docs/examples/coding.ts.json"
+  (golden/as-json "docs/examples/coding.ts.json"
                   (fhir-schema->json "docs/examples/fhir-schema/coding.fs.json"))
 
-  (golden/vs-json "docs/examples/resource-with-string.ts.json"
+  (golden/as-json "docs/examples/resource-with-string.ts.json"
                   (fhir-schema->json "docs/examples/fhir-schema/resource-with-string.fs.json"))
 
-  (golden/vs-json "docs/examples/resource-with-choice.ts.json"
+  (golden/as-json "docs/examples/resource-with-choice.ts.json"
                   (fhir-schema->json "docs/examples/fhir-schema/resource-with-choice.fs.json"))
 
-  (golden/vs-json "docs/examples/resource-with-nested-type.ts.json"
+  (golden/as-json "docs/examples/resource-with-nested-type.ts.json"
                   (fhir-schema->json "docs/examples/fhir-schema/resource-with-nested-type.fs.json"))
 
-  (golden/vs-json "docs/examples/resource-with-codable-concept.ts.json"
+  (golden/as-json "docs/examples/resource-with-codable-concept.ts.json"
                   (fhir-schema->json "docs/examples/fhir-schema/resource-with-codable-concept.fs.json"))
 
-  (golden/vs-json "docs/examples/resource-with-code.ts.json"
+  (golden/as-json "docs/examples/resource-with-code.ts.json"
                   (fhir-schema->json "docs/examples/fhir-schema/resource-with-code.fs.json"))
 
-  (golden/vs-json "test/golden/patient.ts.json"
+  (golden/as-json "test/golden/patient.ts.json"
                   (fhir-schema->json "test/golden/patient.fs.json"))
 
-  (golden/vs-json "test/golden/capability-statement.ts.json"
+  (golden/as-json "test/golden/capability-statement.ts.json"
                   (fhir-schema->json "test/golden/capability-statement.fs.json"))
 
-  (golden/vs-json "test/golden/questionnaire.ts.json"
+  (golden/as-json "test/golden/questionnaire.ts.json"
                   (fhir-schema->json "test/golden/questionnaire.fs.json")))
 
 (defn value-set->json [fhir-schema-file]
@@ -68,13 +68,13 @@
 (deftest value-set->type-schema-golden-test
   (package/init-from-package! "hl7.fhir.r4.core")
 
-  (golden/vs-json "docs/examples/administrative-gender.ts.json"
+  (golden/as-json "docs/examples/administrative-gender.ts.json"
                   (value-set->json "docs/examples/value-set/administrative-gender.json"))
 
-  (golden/vs-json "docs/examples/all-languages.ts.json"
+  (golden/as-json "docs/examples/all-languages.ts.json"
                   (value-set->json "docs/examples/value-set/all-languages.json"))
 
-  (golden/vs-json "docs/examples/marital-status.ts.json"
+  (golden/as-json "docs/examples/marital-status.ts.json"
                   (value-set->json "docs/examples/value-set/marital-status.json")))
 
 (comment
