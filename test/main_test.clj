7d8db16f24cfc278d6d6af87cf39f1bc4fb4e263
Aleksandr Penskoi <alexandr.penskoi@health-samurai.io>
2025-05-01 09:37:52 +0200

    Restructure golden tests.

--- test/main_test.clj
+++ test/main_test.clj
@@ -1,9 +1,33 @@
 (ns main-test
   (:require
    [clojure.test :refer [deftest is]]
-   [main]))
+   [golden.core :as golden]
+   [main]
+   [type-schema.core-test :refer [fhir-schema->type-schemas]]
+   [type-schema.package-index :as package]))
 
 (deftest smoke-test
   (is (= :ok (main/process-package "hl7.fhir.r4.core@4.0.1" "output")))
   (is (= :ok (main/process-package "hl7.fhir.r5.core" "output")))
   (is (= :ok (main/process-package "hl7.fhir.us.core@6.1.0" "output"))))
+
+(deftest fhir-schema->type-schema-realworld-golden-test
+  (package/init-from-package! "hl7.fhir.r4.core")
+
+  (golden/as-jsons ["test/golden/element/element.ts.json"]
+                   (fhir-schema->type-schemas "test/golden/element/element.fs.json"))
+
+  (golden/as-jsons ["test/golden/backbone-element/backbone-element.ts.json"]
+                   (fhir-schema->type-schemas "test/golden/backbone-element/backbone-element.fs.json"))
+
+  (golden/as-jsons ["test/golden/bundle/bundle.ts.json"]
+                   (fhir-schema->type-schemas "test/golden/bundle/bundle.fs.json"))
+
+  (golden/as-jsons ["test/golden/patient/patient.ts.json"]
+                   (fhir-schema->type-schemas "test/golden/patient/patient.fs.json"))
+
+  (golden/as-jsons ["test/golden/capability-statement/capability-statement.ts.json"]
+                   (fhir-schema->type-schemas "test/golden/capability-statement/capability-statement.fs.json"))
+
+  (golden/as-jsons ["test/golden/questionnaire/questionnaire.ts.json"]
+                   (fhir-schema->type-schemas "test/golden/questionnaire/questionnaire.fs.json")))
--- test/type_schema/core_test.clj
+++ test/type_schema/core_test.clj
@@ -13,24 +13,19 @@
                                   ["type"]
                                   {}))))
 
+(defn fhir-schema->type-schemas [fhir-schema-file]
+  (-> (slurp fhir-schema-file)
+      (json/parse-string true)
+      (type-schema/translate)))
+
 (defn fhir-schema->type-schema [fhir-schema-file]
-  (let [type-schemas (-> (slurp fhir-schema-file)
-                         (json/parse-string true)
-                         (type-schema/translate))]
-    (assert (= (count type-schemas) 1))
+  (let [type-schemas (fhir-schema->type-schemas fhir-schema-file)]
+    (assert (= (count type-schemas) 1) (str "Expected 1 type schema, but found " (count type-schemas)
+                                            " type schemas in " fhir-schema-file))
     (first type-schemas)))
 
-(deftest fhir-schema->type-schema-golden-test
+(deftest fhir-schema->type-schema-small-golden-test
   (package/init-from-package! "hl7.fhir.r4.core")
-
-  (golden/as-json "test/golden/element.ts.json"
-                  (fhir-schema->type-schema "test/golden/element.fs.json"))
-
-  (golden/as-json "test/golden/backbone-element.ts.json"
-                  (fhir-schema->type-schema "test/golden/backbone-element.fs.json"))
-
-  (golden/as-json "test/golden/bundle.ts.json"
-                  (fhir-schema->type-schema "test/golden/bundle.fs.json"))
 
   (golden/as-json "docs/examples/string.ts.json"
                   (fhir-schema->type-schema "docs/examples/fhir-schema/string.fs.json"))
@@ -47,20 +42,11 @@
   (golden/as-json "docs/examples/resource-with-nested-type.ts.json"
                   (fhir-schema->type-schema "docs/examples/fhir-schema/resource-with-nested-type.fs.json"))
 
-  (golden/as-json "docs/examples/resource-with-codable-concept.ts.json"
-                  (fhir-schema->type-schema "docs/examples/fhir-schema/resource-with-codable-concept.fs.json"))
+  (golden/as-jsons ["docs/examples/resource-with-codable-concept.ts.json"]
+                   (fhir-schema->type-schemas "docs/examples/fhir-schema/resource-with-codable-concept.fs.json"))
 
-  (golden/as-json "docs/examples/resource-with-code.ts.json"
-                  (fhir-schema->type-schema "docs/examples/fhir-schema/resource-with-code.fs.json"))
-
-  (golden/as-json "test/golden/patient.ts.json"
-                  (fhir-schema->type-schema "test/golden/patient.fs.json"))
-
-  (golden/as-json "test/golden/capability-statement.ts.json"
-                  (fhir-schema->type-schema "test/golden/capability-statement.fs.json"))
-
-  (golden/as-json "test/golden/questionnaire.ts.json"
-                  (fhir-schema->type-schema "test/golden/questionnaire.fs.json")))
+  (golden/as-jsons ["docs/examples/resource-with-code.ts.json"]
+                   (fhir-schema->type-schemas "docs/examples/fhir-schema/resource-with-code.fs.json")))
 
 (defn value-set->json [fhir-schema-file]
   (-> (slurp fhir-schema-file)
@@ -80,8 +66,9 @@
                   (value-set->json "docs/examples/value-set/marital-status.json")))
 
 (comment
+  (clojure.test/run-tests)
   (package/fhir-schema-index "MolecularSequence")
-  (-> (transpiler.fhir-schema/translate
+  (-> (fhir.schema.translate/translate
        (package/index "http://hl7.org/fhir/StructureDefinition/MolecularSequence"))
       (type-schema/translate-fhir-schema))
 
